6e7e835a168a4c248d59cb3bf2fcccd92d92abe6
oppongbaah <oppongbaahisaacmega@gmail.com>
2021-03-11 15:12:31 +0000

    Added a build version

---
+++ wouldyourather/build/static/js/14.28c301a9.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptwitter=this.webpackJsonptwitter||[]).push([[14],{135:function(t,e,n){"use strict";n.r(e);n(1);var r=n(14),c=n(2);e.default=function(){return Object(c.jsxs)(c.Fragment,{children:[Object(c.jsx)("h1",{children:" Page Not Found "}),Object(c.jsx)(r.b,{to:{pathname:"/"},children:"Return Home"})]})}}}]);
+//# sourceMappingURL=14.28c301a9.chunk.js.map
